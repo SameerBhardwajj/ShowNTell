121eedfb8f4923791ade4dd4d613c2752319ebd1
sameer.bhardwaj <sameer.bhardwaj@appinventiv.com>
2020-05-02 15:57:30 +0530

    adding chnages to sprint1

--- src/modules/Chat/Chat.tsx
+++ src/modules/Chat/Chat.tsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import * as React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-export interface AppProps {}
+export interface AppProps {
+  navigation?: any;
+}
 
 export default function App(props: AppProps) {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Chat</Text>
-    </View>
+    <TouchableOpacity
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      onPress={() => props.navigation.navigate("Home")}
+    >
+      <Text>Press To Go Back</Text>
+    </TouchableOpacity>
   );
 }
