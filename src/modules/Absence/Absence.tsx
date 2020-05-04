6a60b9536f24fb6c8816519437564d4c4a039d30
sameer.bhardwaj <sameer.bhardwaj@appinventiv.com>
2020-05-04 10:24:40 +0530

    commit before merge

--- src/modules/Absence/Absence.tsx
+++ src/modules/Absence/Absence.tsx
@@ -1,17 +1,139 @@
 import * as React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
-export interface AppProps {
-  navigation?: any;
-}
+// custom imports
+import { CustomHeader } from "../../Components";
+import { Strings, vw, vh, Images, Colors } from "../../utils";
+import AbsenceFlatlist from "./AbsenceFlatlist";
+
+export interface AppProps {}
 
 export default function App(props: AppProps) {
+  const renderItems = (rowData: any) => {
+    const { item, index } = rowData;
+    return <AbsenceFlatlist item={item} index={index} />;
+  };
+
   return (
-    <TouchableOpacity
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-      onPress={() => props.navigation.navigate("Home")}
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      bounces={false}
+      contentContainerStyle={Styles.mainView}
     >
-      <Text>Press To Go Back</Text>
-    </TouchableOpacity>
+      {/* Custom Header -------------- */}
+      <CustomHeader
+        hideBackButton={true}
+        title={Strings.Absence_Notification}
+        onPressBack={() => {}}
+        textStyle={{ alignSelf: "flex-start", paddingLeft: vw(16) }}
+      />
+      <TouchableOpacity activeOpacity={0.8} style={Styles.childHeader}>
+        <Text style={Styles.childHeaderText}>Alex </Text>
+        <Image source={Images.Drop_Down_icon} style={Styles.dropdown} />
+      </TouchableOpacity>
+
+      {/* Add Btn ----------- */}
+      <TouchableOpacity style={Styles.addBtnView}>
+        <Image source={Images.Add_leave} style={Styles.addBtn} />
+      </TouchableOpacity>
+
+      {/* Message starts here -------- */}
+      <View style={Styles.innerView}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          bounces={false}
+          data={DATA}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={renderItems}
+        />
+      </View>
+    </ScrollView>
   );
 }
+export const Styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  childHeader: {
+    flexDirection: "row",
+    position: "absolute",
+    right: vw(16),
+    top: vh(43),
+    paddingVertical: vw(3),
+    paddingHorizontal: vw(10),
+    backgroundColor: "white",
+    borderRadius: vh(20),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  childHeaderText: {
+    fontFamily: "Nunito-Bold",
+    fontSize: vh(14),
+  },
+  dropdown: {
+    height: vh(6),
+    width: vh(11),
+    marginLeft: vw(5),
+    marginTop: vh(2),
+    tintColor: Colors.violet,
+  },
+  innerView: {
+    paddingHorizontal: vh(16),
+    paddingTop: vh(20),
+    paddingBottom: vh(80),
+    width: "100%",
+  },
+  addBtnView: {
+    position: "absolute",
+    bottom: vh(24),
+    right: vh(24),
+    zIndex: 99,
+  },
+  addBtn: {
+    height: vh(64),
+    width: vh(64),
+  },
+});
+
+const DATA = [
+  {
+    from: "Feb 26, 2020",
+    to: "Feb 26, 2020",
+    name: "Alex Parish",
+    class: "Infant A",
+    msg:
+      "Hello Mam,\n\nThis is to inform you that my ward would be travelling outside station and would be unavailable for the mentioned dates.\nThanks,\nBob",
+    createdOn: "Feb 25, 2020",
+    time: "05:00 PM",
+  },
+  {
+    from: "Feb 26, 2020",
+    to: "Feb 26, 2020",
+    name: "Alex Parish",
+    class: "Infant A",
+    msg:
+      "Hello Mam,\n\nThis is to inform you that my ward would be travelling outside station and would be unavailable for the mentioned dates.\nThanks,\nBob",
+    createdOn: "Feb 25, 2020",
+    time: "05:00 PM",
+  },
+  {
+    from: "Feb 26, 2020",
+    to: "Feb 26, 2020",
+    name: "Alex Parish",
+    class: "Infant A",
+    msg:
+      "Hello Mam,\n\nThis is to inform you that my ward would be travelling outside station and would be unavailable for the mentioned dates.\nThanks,\nBob",
+    createdOn: "Feb 25, 2020",
+    time: "05:00 PM",
+  },
+];
