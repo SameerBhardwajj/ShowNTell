bddcafc171d05ea86f776c9890e2a4dfd6617c07
Appinventiv <appinventiv@Appinventivs-MacBook-Pro.local>
2020-07-12 13:51:25 +0530

    bugs fixing

---
+++ ios/DeviceToken/NSObject+DeviceToken.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+DeviceToken.h
+//  ParentApp
+//
+//  Created by Appinventiv on 09/07/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject ()
+
+@end
+
+NS_ASSUME_NONNULL_END
